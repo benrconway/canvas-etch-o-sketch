0d3ea8d596ccce9c82bc41ed66362e298fd36161
Nicholas Holloway <nholloway91@gmail.com>
2017-10-19 13:02:02 +0100

    line draws dependent upon key press

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ var app = function() {
   var canvas = document.getElementById('main-canvas');
   var context = canvas.getContext('2d');
 
-  var xRef = 250;
-  var yRef = 300;
+  var xRef = 300;
+  var yRef = 250;
 
   var drawFunction = function(x, y){
     context.beginPath()
-    context.moveTo(x, y)
-
-
-
+    context.moveTo(xRef, yRef)
+    context.lineTo(x, y);
+    context.stroke();
   }
 
 
@@ -21,26 +20,28 @@ var app = function() {
 
     //if we press up, decrease x, down increase x
     if(event.keyCode === 38 && movingX !== 0){
-      movingX -= 5;
+      movingY -= 5;
     };
-    if(event.keyCode === 40 && movingX !== 600){
-      movingX += 5;
+    // down
+    if(event.keyCode === 40 && movingX < 600){
+      movingY += 5;
     };
+    // left
     if(event.keyCode === 37 && movingY !== 0){
       movingX -= 5;
     };
-    if(event.keyCode === 39 && movingY !== 500){
+    // right
+    if(event.keyCode === 39 && movingY < 500){
       movingX += 5;
     };
+    drawFunction(movingX, movingY);
     xRef = movingX;
     yRef = movingY;
-    console.log(movingX);
-    //left decrease y, right increases y
-    // and appropriate combinations.
-    // up
-    // console.log(event);
+    // console.log("keycode:", event.keyCode);
+    // console.log("x: ", movingX);
+    // console.log("y: ", movingY);
 
-    // drawFunction();
+
     //change x & y ref to represent stop point.
   })
 }
